c426147497deca783edd3603ca5b3350a1e939eb
Candace Wilson <candaceyw@gmail.com>
2020-04-23 21:42:04 -0400

    final class code

--- fruits/fruits-router.js
+++ fruits/fruits-router.js
@@ -1,47 +1,39 @@
-const express = require("express")
-const knex = require("knex")
+const express = require('express');
+const db = require('../data/config');
 
-const db = knex({
-	client: "sqlite3",
-	useNullAsDefault: true,
-	connection: {
-		filename: "./data/produce.db3",
-	},
-})
+const router = express.Router();
 
-const router = express.Router()
-
-router.get("/", async (req, res, next) => {
+router.get('/', async (req, res, next) => {
 	try {
-		const fruits = await db("fruits")
-		
-		res.json(fruits)
-	} catch(err) {
-		next(err)
-	}
-})
+		const fruits = await db('fruits');
 
-router.get("/:id", async (req, res, next) => {
+		res.json(fruits);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.get('/:id', async (req, res, next) => {
 	try {
-		const { id } = req.params
-		const fruit = await db("fruits").where({ id }).first()
-		
-		res.json(fruit)
-	} catch(err) {
-		next(err)
-	}
-})
+		const { id } = req.params;
+		const fruit = await db('fruits').where({ id }).first();
 
-router.post("/", async (req, res, next) => {
+		res.json(fruit);
+	} catch (err) {
+		next(err);
+	}
+});
+
+router.post('/', async (req, res, next) => {
 	try {
-		const fruitData = req.body
-		const [id] = await db("fruits").insert(fruitData)
-		const newFruit = await db("fruits").where({ id })
+		const fruitData = req.body;
+		const [id] = await db('fruits').insert(fruitData);
+		const newFruit = await db('fruits').where({ id });
 
-		res.status(201).json(newFruit)
-	} catch(err) {
-		next(err)
+		res.status(201).json(newFruit);
+	} catch (err) {
+		next(err);
 	}
-})
+});
 
-module.exports = router
+module.exports = router;
---
+++ knexfile.js
@@ -0,0 +1,9 @@
+// Update with your config settings.
+
+module.exports = {
+	client: 'sqlite3', // specifying the DBMS
+	useNullAsDefault: true, // a flag required for SQLite specifically
+	connection: {
+		filename: './data/produce.db3', // location of our database file
+	},
+};
